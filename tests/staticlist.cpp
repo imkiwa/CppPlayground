65ea17e1b649e9366f911a50d5f5886519fc7224
imkiva <libkernelpanic@gmail.com>
2019-12-27 23:03:39 +0800

    staticlist: update test

--- tests/staticlist.cpp
+++ tests/staticlist.cpp
@@ -9,17 +9,16 @@ int main() {
 
     using t1 = StaticList::List<int, 1, 2, 3>;
     using t2 = StaticList::List<char, 'a', 'b', 'c', 'd'>;
-    printf("%zd\n", StaticList::size_v<t1>);
-    printf("%zd\n", StaticList::size_v<t2>);
+    static_assert(StaticList::size_v<t1> == 3, "You wrote a bug");
+    static_assert(StaticList::size_v<t2> == 4, "You wrote a bug");
 
-    printf("t1[0]: %d\n", StaticList::head<t1>::value);
-    printf("t1[2]: %d\n", StaticList::visit<t1, 2>::value);
+    static_assert(StaticList::head<t1>::value == 1, "You wrote a bug");
+    static_assert(StaticList::visit<t1, 2>::value == 3, "You wrote a bug");
 
     using d0 = StaticList::List<int, 2, 3, 9, 1, 8, 8>;
     using d1 = StaticList::qsort<d0>;
 
     using expected = StaticList::List<int, 1, 2, 3, 8, 8, 9>;
-
     static_assert(StaticList::equals<d1, expected>::value,
         "You wrote a bug");
 }
