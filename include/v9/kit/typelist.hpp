59d9b0a65c793455dc39a58c1b743fe2190b42b6
imkiva <libkernelpanic@gmail.com>
2019-12-27 21:06:19 +0800

    typelist: add constexpr value for short

--- include/v9/kit/typelist.hpp
+++ include/v9/kit/typelist.hpp
@@ -10,42 +10,42 @@ namespace v9::kit {
     struct TypeList {
     private:
         template <typename Seq, typename T>
-        struct TypeListCons;
+        struct Cons;
 
         template <typename Seq>
-        struct TypeListHead;
+        struct Head;
 
         template <typename Seq>
-        struct TypeListTail;
+        struct Tail;
 
         template <typename SeqL, typename SeqR>
-        struct TypeListConcat;
+        struct Concat;
 
         template <typename Seq>
-        struct TypeListSize;
+        struct Size;
 
         template <typename T, template <typename...> class Seq, typename... Ts>
-        struct TypeListCons<T, Seq<Ts...>> {
+        struct Cons<T, Seq<Ts...>> {
             using type = Seq<T, Ts...>;
         };
 
         template <template <typename...> class Seq, typename T, typename... Ts>
-        struct TypeListHead<Seq<T, Ts...>> {
+        struct Head<Seq<T, Ts...>> {
             using type = T;
         };
 
         template <template <typename...> class Seq, typename T, typename... Ts>
-        struct TypeListTail<Seq<T, Ts...>> {
+        struct Tail<Seq<T, Ts...>> {
             using type = Seq<Ts...>;
         };
 
         template <template <typename...> class Seq, typename... Ts, typename ... Us>
-        struct TypeListConcat<Seq<Ts...>, Seq<Us ...>> {
+        struct Concat<Seq<Ts...>, Seq<Us ...>> {
             using type = Seq<Ts..., Us...>;
         };
 
         template <template <typename...> typename Seq, typename... Ts>
-        struct TypeListSize<Seq<Ts...>> {
+        struct Size<Seq<Ts...>> {
             using type = std::integral_constant<std::size_t, sizeof...(Ts)>;
         };
 
@@ -56,21 +56,33 @@ namespace v9::kit {
         using Empty = List<>;
 
         template <typename T, typename Seq>
-        using cons = typename TypeListCons<T, Seq>::type;
+        using cons = typename Cons<T, Seq>::type;
 
         template <typename Seq>
-        using head = typename TypeListHead<Seq>::type;
+        using head = typename Head<Seq>::type;
 
         template <typename Seq>
-        using tail = typename TypeListTail<Seq>::type;
+        using tail = typename Tail<Seq>::type;
 
         template <typename SeqL, typename SeqR>
-        using concat = typename TypeListConcat<SeqL, SeqR>::type;
+        using concat = typename Concat<SeqL, SeqR>::type;
 
         template <typename Seq>
-        using size = typename TypeListSize<Seq>::type;
+        using size = typename Size<Seq>::type;
+
+        template <typename Seq>
+        static constexpr size_t size_v = size<Seq>::value;
+
+        template <typename Seq>
+        using empty = std::is_same<size<Seq>, std::integral_constant<size_t, 0>>;
+
+        template <typename Seq>
+        static constexpr bool empty_v = empty<Seq>::value;
 
         template <typename SeqL, typename SeqR>
         using equals = std::is_same<SeqL, SeqR>;
+
+        template <typename SeqL, typename SeqR>
+        static constexpr bool equals_v = equals<SeqL, SeqR>::value;
     };
 }
