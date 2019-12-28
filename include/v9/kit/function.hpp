06432df4b595b8655d0cf258926e5172793a6fbf
imkiva <libkernelpanic@gmail.com>
2019-12-29 02:20:04 +0800

    function: support all kinds of callable, including lambdas

--- include/v9/kit/function.hpp
+++ include/v9/kit/function.hpp
@@ -39,27 +39,8 @@ namespace v9::kit {
         using Type = typename QualifierRemover<Args...>::Type;
     };
 
-    /**
-     * Parse function typename to return type and argument type(s).
-     * @tparam F function typename
-     */
-    template <typename F>
-    struct FunctionParser : public FunctionParser<decltype(&F::operator())> {
-    };
-
-    /**
-     * Base condition of recursion, extracting all information from functions.
-     *
-     * @tparam Class function owner
-     * @tparam R function return type
-     * @tparam Args function argument type(s)
-     */
-    template <typename Class, typename R, typename... Args>
-    struct FunctionParser<R(Class::*)(Args...) const> {
-        using FunctionType = FunctionAlias<R(Args...)>;
-        using ReturnType = R;
-        using ClassType = Class;
-
+    template <typename ... Args>
+    struct ArgTypeInfo {
         /**
          * Raw argument types, unmodified.
          */
@@ -73,14 +54,84 @@ namespace v9::kit {
     };
 
     /**
-     * Parse all function pointers, including global functions,
-     * static class functions.
-     * Note that, instance methods are not supported.
-     *
-     * @tparam P function pointer type
+     * Parse callable object and lambdas.
+     * @tparam F functor
+     */
+    template <typename F>
+    struct FunctorParser : public FunctorParser<decltype(&F::operator())> {
+    };
+
+    /**
+     * Base condition of recursive, extracting all information from a function.
+     */
+    template <typename Class, typename R, typename... Args>
+    struct FunctorParser<R(Class::*)(Args...) const> : public ArgTypeInfo<Args...> {
+        using FunctionType = FunctionAlias<R(Args...)>;
+        using ReturnType = R;
+        using ClassType = Class;
+    };
+
+    /**
+     * Parse function typename to return type and argument type(s).
+     * @tparam F function typename
+     */
+    template <typename F>
+    struct FunctionParser : public FunctorParser<F> {
+    };
+
+    /**
+     * Parse all function pointers, including
+     * global functions, static class functions.
      */
     template <typename P>
-    struct FunctionParser<P *> : public FunctionParser<FunctionAlias<P>> {
+    struct FunctionParser<P *> : public FunctionParser<P> {
+    };
+
+    /**
+     * Parse function (pointer) lvalue references.
+     */
+    template <typename F>
+    struct FunctionParser<F &> : public FunctionParser<F> {
+    };
+
+    /**
+     * Parse function (pointer) rvalue references.
+     */
+    template <typename F>
+    struct FunctionParser<F &&> : public FunctionParser<F> {
+    };
+
+    /**
+     * Parse instance methods by converting signature to normal form.
+     */
+    template <typename Class, typename R, typename... Args>
+    struct FunctionParser<R(Class::*)(Args...)> : public FunctionParser<R(Class &, Args...)> {
+        using ClassType = Class;
+    };
+
+    /**
+     * Parse const instance methods by converting signature to normal form.
+     */
+    template <typename Class, typename R, typename... Args>
+    struct FunctionParser<R(Class::*)(Args...) const> : public FunctionParser<R(const Class &, Args...) const> {
+        using ClassType = Class;
+    };
+
+    /**
+     * Parse a normal function by converting signature to const normal form.
+     */
+    template <typename R, typename... Args>
+    struct FunctionParser<R(Args...)> : public FunctionParser<R(Args...) const> {
+    };
+
+    /**
+     * Base condition of recursion, extracting all information
+     * from a normal function signature.
+     */
+    template <typename R, typename... Args>
+    struct FunctionParser<R(Args...) const> : public ArgTypeInfo<Args...> {
+        using FunctionType = FunctionAlias<R(Args...)>;
+        using ReturnType = R;
     };
 
     /**
@@ -91,6 +142,7 @@ namespace v9::kit {
 
     /**
      * Convert callable things to function type (aka FunctionAlias).
+     * Sample usage: {@code auto f = makeFunction(sth); }
      *
      * @see FunctionAlias
      * @tparam F Function typename
