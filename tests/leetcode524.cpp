5cae4546a239e3d0dc773f4d786a6b81e0069453
imkiva <libkernelpanic@gmail.com>
2020-03-12 23:49:48 +0800

    leetcode: 524

--- tests/leetcode524.cpp
+++ tests/leetcode524.cpp
@@ -14,17 +14,26 @@ std::string longest(const std::string &src, const std::vector<std::string> &dict
     for (auto &&elem : dict) {
         auto l = src.c_str();
         auto r = elem.c_str();
+
+        // iterate src and dict
         while (*l && *r) {
             if (*l == *r) {
+                // if current char are the same,
+                // we match this char
                 ++l;
                 ++r;
             } else {
+                // or we just delete this char from src,
+                // try to match elem with the next char in src
                 ++l;
             }
         }
 
-        // matched the whole src using elem
+        // successfully matched the elem after deleting some chars in src,
+        // the elem has been matched, r now points to the '\0',
         if (!*r) {
+            // if this match is longer than previous one,
+            // replace it with current one.
             int match = r - elem.c_str();
             if (match > longest_match) {
                 longest_match = match;
